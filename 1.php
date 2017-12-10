2eeddc2aee0a076ae1a95208df1617f15ad126b7
Toney <32724492+Toney-SCH@users.noreply.github.com>
2017-12-10 22:28:41 +0800

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,28 @@
+
+<?php
+* the declaration of a string (data type)
+*
+* 1. can use double quotes or single quotes
+*
+* the variable can be parsed in the double quotes, and all the escape characters can be used in the double quotes
+*
+* 2. < < <
+*
+* segment, match, find, replace
+*
+*
+* feature: if it is other types of data, you can also use a string processing function. The other types are automatically converted to a self - character string and reprocessed
+* string can be accessed to each character by the subscript, like an array. (but not array array can also be accessed by members of the subscript {})
+*
+
+* in addition to English characters, there are Chinese
+
+* /
+//	echo count("abc");
+
+
+	$str = "hello";
+
+	$str[2]="world";
+
+	var_dump($str);
